48450f2ac2501310697a68e25d949f0cc943f9b3
Vyacheslav Kirizleev <slava91274@gmail.com>
2020-08-19 21:35:25 +0300

    task2 added

---
+++ src/com/sevaslk/javacore/chapter28/task2/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.sevaslk.javacore.chapter28.task2;
+
+
+import java.util.stream.IntStream;
+
+class FizzBuzz {
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 50).parallel()
+                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+                .forEachOrdered(System.out::println);
+    }
+}
